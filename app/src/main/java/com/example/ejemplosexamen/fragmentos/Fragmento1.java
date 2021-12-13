cc93273f6d92d90ba0ba48b96a6cff56f2f0ea1c
BSmithMarin <brahyansmithm@gmail.com>
2021-12-13 10:40:57 +0100

    creado toast personalizado

--- app/src/main/java/com/example/ejemplosexamen/fragmentos/Fragmento1.java
+++ app/src/main/java/com/example/ejemplosexamen/fragmentos/Fragmento1.java
@@ -1,5 +1,6 @@
 package com.example.ejemplosexamen.fragmentos;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -48,11 +49,28 @@ public class Fragmento1 extends Fragment implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
-        if (v == btnToast) {
+        if (v == btnToast) {//Toas por defecto
             Toast toast = Toast.makeText(requireActivity().getApplicationContext(), "Mensaje Toast", Toast.LENGTH_SHORT);
             toast.setGravity(Gravity.TOP, 0, 0);
             toast.show();
+
+            toastPersonalizado(); //Ejecucion de ambos toast simultaneos
         }
+    }
+
+    private void toastPersonalizado(){
+        //Dos formas de consegir un Layout inflates, el asociado a la clase principal, o traves de un servicio
+        //Del sistema
+        LayoutInflater inflater = requireActivity().getLayoutInflater();
+        LayoutInflater inflater2 = (LayoutInflater) requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View toastPersonalizado = inflater.inflate(R.layout.custom_toast,null);
+        //Atributos del toast
+        Toast toast = new Toast(requireContext());
+        toast.setGravity(Gravity.CENTER_VERTICAL,0,0);
+        toast.setDuration(Toast.LENGTH_LONG);
+        toast.setView(toastPersonalizado);
+        toast.show();//Muestra el toast
     }
 
 }
